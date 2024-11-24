a09196c4e8e8823538fedcff6a7d1ab1df3ed38e
admin-dev-exterview <admindev@exterview.io>
2024-11-25 00:33:49 +0530

    fixed build issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { FaInstagram, FaLinkedin } from "react-icons/fa";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 // import { motion, useScroll, useTransform, useInView } from "motion/react";
 import { useRef } from "react";
 import {
